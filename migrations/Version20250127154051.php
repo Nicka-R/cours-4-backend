cbd331afcc60c0a2d2c6b65b37949d658096429a
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2025-02-08 21:42:29 +0100

    update: remove tiny_puppy table from migration and create personne table

--- migrations/Version20250127154051.php
+++ migrations/Version20250127154051.php
@@ -21,13 +21,11 @@ final class Version20250127154051 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE personne (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, age INTEGER NOT NULL, prenom VARCHAR(255) NOT NULL)');
-        $this->addSql('DROP TABLE tiny_puppy');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE tiny_puppy (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL COLLATE "BINARY", age INTEGER NOT NULL)');
         $this->addSql('DROP TABLE personne');
     }
 }
